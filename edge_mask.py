0d27176950a41ce2360e73e6a5ed1110e394dd38
swastik <swastikgorai@gmail.com>
2023-05-26 10:32:24 +0530

    added edgemask

---
+++ edge_mask.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class EdgeMask:
+
+    def __init__(self, path):
+        self.path = path
+
+    # Read image
+
+    def read_img(self, p):
+        img = cv2.imread(p)
+        assert img is not None, "Image not found in path, check path again"
+        return img
+
+    # Show image
+    def show_img(self, img):
+        plt.imshow(img)
+        plt.subplot(121), plt.imshow(img, cmap='gray')
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122), plt.imshow(edges, cmap='gray')
+        plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+        plt.show()
+
+    # Save image
+    def save_img(self, img, path):
+        cv2.imwrite(path, img)
+
+    # Convert image to grayscale
+    def grayscale(self, img):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    def edge(self, img):
+        return cv2.Canny(img, 100, 200)
